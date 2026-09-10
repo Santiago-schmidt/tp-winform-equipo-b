8757d9b21431fe1210da2fdd59664bf768f9090b
Santiago Schmidt <cololjp@gmail.com>
2026-09-10 14:07:01 -0300

    Agrega clases Artículo e Imagen

---
+++ TPWinForm_equipo-B/Articulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_B
+{
+    internal class Articulo
+    {
+        public Articulo() 
+        {
+            Imagenes = new List<Imagen>();
+        }
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
---
+++ TPWinForm_equipo-B/Imagen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_B
+{
+    internal class Imagen
+    {
+        public int Id { get; set; }
+        public int IdArticulo { get; set; }
+        public string ImagenUrl { get; set; }
+
+    }
+}
