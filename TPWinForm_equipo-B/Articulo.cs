1a10e46e8019dacb2f632fce350a13ad00ddcec4
Santiago Schmidt <cololjp@gmail.com>
2026-09-10 14:21:50 -0300

    Agrega clases Marca y Categoría

--- TPWinForm_equipo-B/Articulo.cs
+++ TPWinForm_equipo-B/Articulo.cs
@@ -16,8 +16,8 @@ namespace TPWinForm_equipo_B
         public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-        public string Marca { get; set; }
-        public string Categoria { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
         public List<Imagen> Imagenes { get; set; }
         public decimal Precio { get; set; }
 
