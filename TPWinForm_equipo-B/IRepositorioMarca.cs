76bf3cf2d1dc2d56c766e5bbe34b5605a4105fbe
lewandmarcos-del <lewand.marcos@gmail.com>
2026-09-14 22:39:12 -0300

    Crear interfaces IRepositorioMarca e IRepositorioCategoria

---
+++ TPWinForm_equipo-B/IRepositorioMarca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_B
+{
+    public interface IRepositorioMarca
+    {
+
+    }
+}
