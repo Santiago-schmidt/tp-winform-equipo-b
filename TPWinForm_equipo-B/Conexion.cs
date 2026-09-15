84a1de97e9e65235d95204d0420b8b608ebb820f
lewandmarcos-del <lewand.marcos@gmail.com>
2026-09-14 23:36:57 -0300

    Conexion con BDD

--- TPWinForm_equipo-B/Conexion.cs
+++ TPWinForm_equipo-B/Conexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,15 @@ namespace TPWinForm_equipo_B
 {
     public static class Conexion
     {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection();
+            conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+
+            conexion.Open();
+
+            return conexion;
+        }
 
     }
 }
