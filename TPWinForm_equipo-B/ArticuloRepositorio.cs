984c3c2285674773884a96a0034e700a2339e11c
lewandmarcos-del <lewand.marcos@gmail.com>
2026-09-17 21:28:56 -0300

    Se eliminó ArticuloBuscador para incluir la busqueda en la clase ArticuluRepositorio

--- TPWinForm_equipo-B/ArticuloRepositorio.cs
+++ TPWinForm_equipo-B/ArticuloRepositorio.cs
@@ -175,5 +175,105 @@ namespace TPWinForm_equipo_B
                 datos.cerrarConexion();
             }
         }
+
+        public List<Articulo> Filtrar(string campo, string criterio, string filtro)
+        {
+            List<Articulo> articulos = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                string consulta = "SELECT a.Id, a.Codigo, a.Nombre, a.Descripcion, a.Precio, m.Id AS IdMarca, m.Descripcion AS DescripcionMarca, c.Id AS IdCategoria, c.Descripcion AS DescripcionCategoria FROM Articulos a LEFT JOIN Marcas m ON m.Id = a.IdMarca LEFT JOIN Categorias c ON c.Id = a.IdCategoria WHERE ";
+
+                if (campo == "Precio")
+                {
+                    switch (criterio)
+                    {
+                        case "Mayor a":
+                            consulta += "a.Precio > " + filtro;
+                            break;
+                        case "Menor a":
+                            consulta += "a.Precio < " + filtro;
+                            break;
+                        default:
+                            consulta += "a.Precio = " + filtro;
+                            break;
+                    }
+                }
+                else if (campo == "Nombre")
+                {
+                    switch (criterio)
+                    {
+                        case "Comienza con":
+                            consulta += "a.Nombre LIKE '" + filtro + "%' ";
+                            break;
+                        case "Termina con":
+                            consulta += "a.Nombre LIKE '%" + filtro + "'";
+                            break;
+                        default:
+                            consulta += "a.Nombre LIKE '%" + filtro + "%'";
+                            break;
+                    }
+                }
+                else
+                {
+                    // Asumimos que la última opción de búsqueda es "Descripción"
+                    switch (criterio)
+                    {
+                        case "Comienza con":
+                            consulta += "a.Descripcion LIKE '" + filtro + "%' ";
+                            break;
+                        case "Termina con":
+                            consulta += "a.Descripcion LIKE '%" + filtro + "'";
+                            break;
+                        default:
+                            consulta += "a.Descripcion LIKE '%" + filtro + "%'";
+                            break;
+                    }
+                }
+
+                datos.setearConsulta(consulta);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Codigo = datos.Lector["Codigo"] is DBNull ? string.Empty : (string)datos.Lector["Codigo"];
+                    aux.Nombre = datos.Lector["Nombre"] is DBNull ? string.Empty : (string)datos.Lector["Nombre"];
+
+                    if (!(datos.Lector["Descripcion"] is DBNull))
+                        aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    aux.Precio = datos.Lector["Precio"] is DBNull ? 0 : (decimal)datos.Lector["Precio"];
+
+                    aux.Marca = new Marca();
+                    if (!(datos.Lector["IdMarca"] is DBNull))
+                    {
+                        aux.Marca.Id = (int)datos.Lector["IdMarca"];
+                        aux.Marca.Descripcion = (string)datos.Lector["DescripcionMarca"];
+                    }
+
+                    aux.Categoria = new Categoria();
+                    if (!(datos.Lector["IdCategoria"] is DBNull))
+                    {
+                        aux.Categoria.Id = (int)datos.Lector["IdCategoria"];
+                        aux.Categoria.Descripcion = (string)datos.Lector["DescripcionCategoria"];
+                    }
+
+                    articulos.Add(aux);
+                }
+
+                return articulos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
