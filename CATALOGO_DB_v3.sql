1a6042550b2365f7d1dd42b369648efbedff8831
Santiago Schmidt <cololjp@gmail.com>
2026-09-20 10:12:51 -0300

    Agrega imagenes de articulos a la base de datos

--- CATALOGO_DB_v3.sql
+++ CATALOGO_DB_v3.sql
@@ -96,12 +96,21 @@ insert into ARTICULOS values ('S01', 'Galaxy S10', 'Una canoa cara', 1, 1, 69.99
 ('S56', 'Bravia 55', 'Alta tele', 3, 2, 49500),
 ('A23', 'Apple TV', 'lindo loro', 2, 3, 7850)
 
+/*TRUNCATE TABLE IMAGENES*/
 insert into imagenes values
-(1,'https://images.samsung.com/is/image/samsung/co-galaxy-s10-sm-g970-sm-g970fzyjcoo-frontcanaryyellow-thumb-149016542'),
-(2, 'https://www.motorola.cl/arquivos/moto-g7-play-img-product.png?v=636862863804700000'),
-(2, 'https://i.blogs.es/9da288/moto-g7-/1366_2000.jpg'),
-(3, 'https://www.euronics.cz/image/product/800x800/532620.jpg'),
-(4, 'https://intercompras.com/product_thumb_keepratio_2.php?img=images/product/SONY_KDL-55W950A.jpg&w=650&h=450'),
-(5, 'https://cnnespanol2.files.wordpress.com/2015/12/gadgets-mc3a1s-populares-apple-tv-2015-18.jpg?quality=100&strip=info&w=460&h=260&crop=1')
+(1, 'https://i.blogs.es/addc23/samsung-galaxy-s10/650_1200.jpg'),
+(1, 'https://i.blogs.es/e5b8d4/galaxy-s10-camaras/450_1000.jpg'),
+(1, 'https://img.global.news.samsung.com/ar/wp-content/uploads/2019/03/S10-FB-ESP-v2.jpg'),
+(2, 'https://http2.mlstatic.com/D_NQ_NP_806739-MLA79776344638_102024-O.webp'),
+(2, 'https://http2.mlstatic.com/D_Q_NP_2X_796177-CBT109115309977_032026-T.webp'),
+(2, 'https://jumboargentina.vtexassets.com/arquivos/ids/548647-800-auto?v=637045464536970000&width=800&height=auto&aspect=true'),
+(3, 'https://http2.mlstatic.com/D_NQ_NP_786815-MLA74779490077_022024-O.webp'),
+(3, 'https://thumbs.ielectro.es/product/med/96278-1.webp'),
+(4, 'https://images.fravega.com/f1000/e727b0ddef5598e45fd1410a021ee0cb.jpg'),
+(4, 'https://http2.mlstatic.com/D_NQ_NP_893994-MLA89649715904_082025-O.webp'),
+(4, 'https://images.fravega.com/f1000/524d7e931a1ae681c378faf17caee6e4.jpg'),
+(5, 'https://cdn.eldestapeweb.com/eldestape/022026/1771420010961/the-morning-show-en-televisor-jpg..jpg?cw=1200&ch=675'),
+(5, 'https://cdn.eldestapeweb.com/eldestape/022026/1771420009206/apple-tv-catalogo-jpg..webp?cw=720&ch=405&extw=jpg'),
+(5, 'https://www.lg.com/ar/images/Microsite/apple-tv-plus-2025-1st/apple-tv-plus-2025-microsite-easy-to-redeem-m.jpg')
 
 select * from ARTICULOS
